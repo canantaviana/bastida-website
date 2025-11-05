1f6ee8d0a2cf0e86a12f6b82475069202c19a6c1
Carles Briansó <carles.brianso@antaviana.cat>
2025-11-05 11:59:23 +0100

    Cambiado css portada

--- tpl/main_home/main_home.php
+++ tpl/main_home/main_home.php
@@ -1,5 +1,9 @@
 <?php
 
+
+array_pop(page::$css_ar_url);
+page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/css/bastida-home.css';
+
 // main_home
 	$title 			= $this->get_element_from_template_map('title', $template_map->{$mode});
 	$abstract  		= $this->get_element_from_template_map('abstract', $template_map->{$mode});
